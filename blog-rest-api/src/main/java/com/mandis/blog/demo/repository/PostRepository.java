232f33da316596212c8188ab0e5a776c26a1f545
mandis-ncs <amnd.castro@gmail.com>
2023-08-01 13:36:30 -0300

    final sec 9 mapper

--- blog-rest-api/src/main/java/com/mandis/blog/demo/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mandis.blog.demo.repository;
-
-import com.mandis.blog.demo.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
