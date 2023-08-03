7d9c24f4e028c9b563004bb117721d3c289abe70
mandis-ncs <amnd.castro@gmail.com>
2023-08-03 14:29:12 -0300

    merging

---
+++ blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -0,0 +1,47 @@
+package com.springboot.blog;
+
+import com.springboot.blog.entity.Role;
+import com.springboot.blog.repository.RoleRepository;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringbootBlogRestApiApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogRestApiApplication.class, args);
+	}
+
+}
+
+/*
+* // Deprecated solution to set Roles
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+
+	}
+* */
